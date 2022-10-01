15bb04eb0ecf06dc4ffb3557f22c99f0df445bd4
voie420 <66523399+voie420@users.noreply.github.com>
2022-10-01 14:40:38 +0200

    Cobblestone Collection & Sounds
    Added the cobblestone 2014 collection. (Inc: Dragon Lore). Also added Shooting sounds and Headshot sounds. Headshots will give you 2x money.

---
+++ BravoCase.js
@@ -0,0 +1,107 @@
+function BravoBlue() {
+    var items = [
+        "G3SG1 | Demeter^./img/Bravo/g3sg1_demeter^6.74^#4b69ff",
+        "Galil AR | Shattered^./img/Bravo/galil_ar_shattered^9.32^#4b69ff",
+        "SG 553 | Wave Spray^./img/Bravo/sg553_wave_spray^10.53^#4b69ff",
+        "Nova | Tempest^./img/Bravo/nova_tempest^7.23^#4b69ff",
+        "UMP-45 | Bone Pile^./img/Bravo/ump_45_bone_pile^11.74^#4b69ff",
+        "Dual Berettas | Black Limba^./img/Bravo/db_black_limba^6.64^#4b69ff"
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+
+
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
+
+function BravoPurple() {
+    var items = [
+        "P90 | Emerald Dragon^./img/Bravo/p90_emerald_dragon^200.53^#8847ff",
+        "USP-S | Overgrowth^./img/Bravo/usps_overgrowth^60.52^#8847ff",
+        "M4A1-S | Bright Water^./img/Bravo/m4a1s_bright_water^32.42^#8847ff",
+        "M4A4 | Zirka^./img/Bravo/m4a4_zirka^45.34^#8847ff",
+        "MAC-10 | Graven^./img/Bravo/mac-10_graven^30.53^#8847ff",
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+    
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
+
+function BravoPink() {
+    var items = [
+        "P2000 | Ocean Foam^./img/Bravo/p2000_ocean_foam^150.43^#d32ee6",
+        "AWP | Graphite^./img/Bravo/awp_graphite^196.64^#d32ee6",
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
+
+function BravoRed() {
+    var items = [
+        "Desert Eagle | Golden Koi^./img/Bravo/deagle_golden_koi^38.42^#eb4b4b",
+        "AK-47 | Fire Serpent^./img/Bravo/ak_47_fire_serpent^2474.53^#eb4b4b",
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+    
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
---
+++ CobblestonePackage.js
@@ -0,0 +1,116 @@
+function CobblestoneBlue() {
+    var items = [
+        "Dual Berettas | Briar^./img/Cobblestone/db_briar^0.23^#b0c3d9",
+        "SCAR-20 | Storm^./img/Cobblestone/scar-20_storm^0.32^#b0c3d9",
+        "P90 | Storm^./img/Cobblestone/p90_storm^0.45^#b0c3d9",
+        "UMP-45 | Indigo^./img/Cobblestone/ump-45_Indigo^0.45^#b0c3d9",
+        "MAC-10 | Indigo^./img/Cobblestone/mac-10_indigo^0.63^#b0c3d9",
+        "Nova | Green Apple^./img/Cobblestone/nova_green_apple^0.95^#5e98d9",
+        "Sawed-Off | Rust Coat^./img/Cobblestone/sawed_rust_coat^1.08^#5e98d9",
+        "MAG-7 | Silver^./img/Cobblestone/mag7_silver^0.95^#5e98d9",
+        "USP-S | Royal Blue^./img/Cobblestone/usps_royal_blue^35.32^#5e98d9",
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+
+
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
+
+function CobblestonePurple() {
+    var items = [
+        "Dual Berettas | Briar^./img/Cobblestone/db_briar^0.23^#b0c3d9",
+        "SCAR-20 | Storm^./img/Cobblestone/scar-20_storm^0.32^#b0c3d9",
+        "P90 | Storm^./img/Cobblestone/p90_storm^0.45^#b0c3d9",
+        "UMP-45 | Indigo^./img/Cobblestone/ump-45_Indigo^0.45^#b0c3d9",
+        "MAC-10 | Indigo^./img/Cobblestone/mac-10_indigo^0.63^#b0c3d9",
+        "Nova | Green Apple^./img/Cobblestone/nova_green_apple^0.95^#5e98d9",
+        "Sawed-Off | Rust Coat^./img/Cobblestone/sawed_rust_coat^1.08^#5e98d9",
+        "MAG-7 | Silver^./img/Cobblestone/mag7_silver^0.95^#5e98d9",
+        "USP-S | Royal Blue^./img/Cobblestone/usps_royal_blue^35.32^#5e98d9",
+        "P2000 | Chainmail^./img/Cobblestone/p2000_chainmail^79.63^#4b69ff",
+        "MP9 | Dark Age^./img/Cobblestone/mp9_dark_age^78.32^#4b69ff"
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+    
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
+
+function CobblestonePink() {
+    var items = [
+        "Desert Eagle | Hand Cannon^./img/Cobblestone/deagle_hand_cannon^874.32^#8847ff",
+        "CZ75-Auto | Chalice^./img/Cobblestone/cz75-auto_chalice^1378.23^#8847ff",
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
+
+function CobblestoneRed() {
+    var items = [
+        "M4A1-S | Knight^./img/Cobblestone/m4a1s_knight^2836.35^#d32ee6",
+        "AWP | Dragon Lore^./img/Cobblestone/awp_dragon_lore^10383.31^#eb4b4b",
+    ]
+
+
+    idforbutton += 1
+    var randomitem = items[Math.floor(Math.random() * items.length)];
+    let iteminfo = randomitem.split("^")
+    let curritems = document.getElementById("itemstext").innerHTML;
+    
+    let state = document.getElementById("popupstate").textContent
+        
+    if (state == "Enabled") {
+        document.getElementById("newitemname").textContent = `${iteminfo[0]}`
+        document.getElementById("newitemimg").src = `${iteminfo[1]}.png`
+        document.getElementById("weaponwin").style.borderColor = `${iteminfo[3]}`
+    
+    
+        document.getElementById("menu").style.visibility = "visible";
+    }
+    document.getElementById("itemstext").innerHTML = `${curritems}<div id=""class="itembg" style="border-style: solid;border-color:${iteminfo[3]};"><button id='${idforbutton}' class="clearbutton" onClick="reply_click(this.id)"><img draggable="false" src=${iteminfo[1]}.png style='height:50px; width:50px;'>$${iteminfo[2]}</button></div>`;
+}
